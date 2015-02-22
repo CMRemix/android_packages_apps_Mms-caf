bd91cf489ae61a7641a0fda45858604c6af0122d
Bajee11 <bajee11@gmail.com>
2015-02-22 02:18:07 -0700

    [2/4] Mms: Breathing missedcall/sms/voicemail
    Base: https://gerrit.omnirom.org/#/c/4976/
    OmniGears: https://gerrit.omnirom.org/#/c/4978/
    Telephony: https://gerrit.omnirom.org/#/c/4979/
    
    Change-Id: Id12182719315a6e5bcbabd8f843eff70c8c3ed3a
    
    @alviteri: Port by me to crDroid Android
    
    Conflicts:
    	res/drawable-hdpi/stat_notify_sms.png
    	res/drawable-mdpi/stat_notify_sms.png
    	res/drawable-xhdpi/stat_notify_sms.png
    	res/drawable-xxhdpi/stat_notify_sms.png
    	src/com/android/mms/transaction/MessagingNotification.java

--- src/com/android/mms/transaction/MessagingNotification.java
+++ src/com/android/mms/transaction/MessagingNotification.java
@@ -57,6 +57,7 @@ import android.os.Parcel;
 import android.os.Parcelable;
 import android.preference.PreferenceManager;
 import android.provider.BaseColumns;
+import android.provider.Settings;
 import android.provider.Telephony.Mms;
 import android.provider.Telephony.MmsSms;
 import android.provider.Telephony.Sms;
@@ -396,7 +397,7 @@ public class MessagingNotification {
         NotificationManager nm = (NotificationManager)
                 context.getSystemService(Context.NOTIFICATION_SERVICE);
 
-//        TaskStackBuilder taskStackBuilder = TaskStackBuilder.create(context);
+        // TaskStackBuilder taskStackBuilder = TaskStackBuilder.create(context);
         // Update the notification.
         PendingIntent pendingIntent;
         if (phoneId == MessageUtils.SUB_INVALID) {
@@ -1177,7 +1178,12 @@ public class MessagingNotification {
                     mostRecentNotification.mClickIntent, PendingIntent.FLAG_UPDATE_CURRENT);
         }
         // Always have to set the small icon or the notification is ignored
-        noti.setSmallIcon(R.drawable.stat_notify_sms);
+        if (Settings.CMREMIX.getInt(context.getContentResolver(),
+                Settings.CMREMIX.KEY_SMS_BREATH, 0) == 1) {
+               noti.setSmallIcon(R.drawable.stat_notify_sms_breath);
+           } else {    
+               noti.setSmallIcon(R.drawable.stat_notify_sms);
+        }
 
         NotificationManagerCompat nm = NotificationManagerCompat.from(context);
 
